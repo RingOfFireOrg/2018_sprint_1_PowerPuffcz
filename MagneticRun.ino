1605c7fda14d6c2fed1f2e2aedf7c8a20644e62c
Fluffyfluff_DragonCat <2012SarahShaw@Gmail.com>
2018-10-25 18:21:04 -0400

    Update and rename BasicRun.ino to MagneticRun.ino

--- MagneticRun.ino
+++ MagneticRun.ino
@@ -1,16 +1,22 @@
 #include<Servo.h>
-int servoPinL=0;
-int servoPinR=1;
+int servoPinL=0;//Left motor pin #
+int servoPinR=1;//Right motor pin #
 Servo servoL;
 Servo servoR;
 
-int sensor=2;
-int LED=3;
+int sensor=2;//MAGENTIC Sensor pin #
+int LED=3;//LED motor pin # (Part of Magnetic sensor)
 int val;
 
 int magCounter=0;
-int maxCount=5;
+int maxCount=5;//max numbers of magnets it needs to sense
 
+/*
+ * This might actually move the robot backwards, so
+ * please check if this code actually moves the robot forward.
+ * If it doesn't change the moveForward to moveBackward and
+ * also change the original moveBackward to MoveForward
+ */
 void moveForward(){
   for(int angle=0; angle<361; angle++){
     servoL.write(angle);
@@ -46,19 +52,14 @@ void setup() {
 }
 
 void loop() {
- boolean detect= detectMag();
- if(magCounter==maxCount){
-  if(){//add case to stop moving back
-    
-  }
-  else{
-    moveBackward();
-  }
- }
- else if(detect){//true
-  delay();
+ boolean detect=detectMag();
+ if(detect){//true
+  delay(200);
   magCounter++;
   moveForward();
+    if(magCounter>=maxCount){
+      milis(500);
+    }
  }
  else{
   moveForward();
